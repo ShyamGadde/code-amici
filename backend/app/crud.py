6e2fb5d875c2629b34cde711a8e2bf80b5cc6705
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-04-14 14:54:30 +0530

    perf(backend): improve get_user_matches performance by removing subquery for each record

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -26,27 +26,32 @@ def get_user_by_email(session: Session, email: str) -> User | None:
 
 
 def get_user_matches(session: Session, current_user: User) -> list[schemas.UserMatch]:
-    # TODO: Use current user parameters directly in WHERE clause conditions instead of a subquery
     query = text("""
         SELECT id, email, full_name, bio, profile_pic_url, dob, gender, github_username, linkedin_username, skills, country, hobbies
-        FROM users u1
-        WHERE EXISTS (
-            SELECT 1
-            FROM users u2
-            WHERE u2.id = :current_user_id AND u1.id != u2.id AND
-                (
-                    (u1.preferred_buddy_type = 'Buddy' AND u2.preferred_buddy_type = 'Buddy') OR
-                    (u1.preferred_buddy_type = 'Mentor' AND u2.preferred_buddy_type = 'Mentee') OR
-                    (u1.preferred_buddy_type = 'Mentee' AND u2.preferred_buddy_type = 'Mentor')
-                )
-                AND u1.skills && u2.preferred_skills
-                AND u2.skills && u1.preferred_skills
-                AND (u1.gender = u2.preferred_gender OR u2.preferred_gender = 'Any')
-                AND (u2.gender = u1.preferred_gender OR u1.preferred_gender = 'Any')
-        )
-        """)
+        FROM users u
+        WHERE u.id != :current_user_id AND
+            (
+                (u.preferred_buddy_type = 'Buddy' AND :current_user_preferred_buddy_type = 'Buddy') OR
+                (u.preferred_buddy_type = 'Mentor' AND :current_user_preferred_buddy_type = 'Mentee') OR
+                (u.preferred_buddy_type = 'Mentee' AND :current_user_preferred_buddy_type = 'Mentor')
+            )
+            AND u.skills && ARRAY[:current_user_preferred_skills]::varchar[]
+            AND ARRAY[:current_user_skills]::varchar[] && u.preferred_skills
+            AND (u.gender = :current_user_preferred_gender OR :current_user_preferred_gender = 'Any')
+            AND (:current_user_gender = u.preferred_gender OR u.preferred_gender = 'Any')
+    """)
 
-    result = session.execute(query, {"current_user_id": current_user.id})
+    result = session.execute(
+        query,
+        {
+            "current_user_id": current_user.id,
+            "current_user_preferred_buddy_type": current_user.preferred_buddy_type,
+            "current_user_preferred_skills": current_user.preferred_skills,
+            "current_user_skills": current_user.skills,
+            "current_user_preferred_gender": current_user.preferred_gender,
+            "current_user_gender": current_user.gender,
+        },
+    )
     matches = [schemas.UserMatch.model_validate(row) for row in result]
     return matches
 
