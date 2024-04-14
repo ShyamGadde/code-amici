94d7339ae6e675a39eb8ae3a18a9e38e891a9ab7
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-04-14 14:25:12 +0530

    refactor(backend): reorder some stuff

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,16 @@ def read_user_me(user_id: int, session: SessionDep) -> Any:
     return db_user
 
 
+@app.get("/users/me/matches/", response_model=list[schemas.UserMatch])
+def get_user_matches(user_id: int, session: SessionDep) -> Any:
+    db_user = crud.get_user(session, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    matches = crud.get_user_matches(session, db_user)
+    matches = rank_matches(db_user, matches)
+    return matches
+
+
 @app.patch("/users/me/", response_model=schemas.UserPublic)
 def update_user_me(
     user_id: int, session: SessionDep, user_in: schemas.UserUpdate
@@ -40,13 +50,3 @@ def update_user_me(
         raise HTTPException(status_code=404, detail="User not found")
     db_user = crud.update_user(session, db_user, user_in)
     return db_user
-
-
-@app.get("/users/me/matches/", response_model=list[schemas.UserMatch])
-def get_user_matches(user_id: int, session: SessionDep) -> Any:
-    db_user = crud.get_user(session, user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    matches = crud.get_user_matches(session, db_user)
-    matches = rank_matches(db_user, matches)
-    return matches
