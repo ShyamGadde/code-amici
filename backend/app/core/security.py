854ca7f0e23c507bddfd2c82df6815c783399622
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-04-17 22:20:22 +0530

    fix(backend): crud operation functions not matching the new field names of the db table

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -64,5 +64,5 @@ def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(plain_password: str, password: str) -> bool:
+    return pwd_context.verify(plain_password, password)
